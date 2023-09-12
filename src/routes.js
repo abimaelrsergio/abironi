8871eaf558492c92a3e2967c16949e8819807908
Abimael Sergio <abimaelsergio@rivian.com>
2023-09-12 08:37:18 -0300

    Second commit

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Sobre from 'pages/Sobre';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/index';
+import NotFound from 'pages/NotFound';
+
+export default function AppRouter() {
+  return (
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />} >
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+  );
+}
